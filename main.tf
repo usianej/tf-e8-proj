0e51d54b3a431093af760839965b5a8a43e29dfc
Jeff <usianeneefferson@gmail.com>
2024-07-25 20:07:15 +0100

    main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.7.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate19531"
+    container_name       = "tfstate2"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "state-of-the-art"
+  location = "eastus"
+}
