1bfa0f0397b89d108b22699c8d1f5bf18d1c61a3
Usianene Jefferson <usianenejefferson@gmail.com>
2024-07-29 21:00:44 +0100

    removed port 80 vulnerability

--- main.tf
+++ main.tf
@@ -50,18 +50,6 @@ resource "azurerm_network_security_group" "main_nsg" {
   resource_group_name = azurerm_resource_group.main_rg.name
 
   security_rule {
-    name                       = "Allow_HTTP"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
     name                       = "Allow_HTTPS"
     priority                   = 200
     direction                  = "Inbound"
